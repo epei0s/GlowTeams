50bafa89abc70d8f84310220109d7cdfea1bbc8a
epei0s <luca@sarwaitz.de>
2020-07-08 00:39:41 +0200

    fix Main

--- src/main/java/de/epeios/GlowTeams/commands/GlowCommand.java
+++ src/main/java/de/epeios/GlowTeams/commands/GlowCommand.java
@@ -1,6 +1,19 @@
 package de.epeios.GlowTeams.commands;
 
-public class GlowCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class GlowCommand implements CommandExecutor {
+
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+
+        return false;
+    }
 
 
 }
+

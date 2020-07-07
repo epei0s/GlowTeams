67a3e3ff3893a79584ecfe98340c65398259e248
epei0s <luca@sarwaitz.de>
2020-07-07 22:39:45 +0200

    fix Main

--- src/main/java/de/epeios/GlowTeams/Main.java
+++ src/main/java/de/epeios/GlowTeams/Main.java
@@ -4,8 +4,5 @@ import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.Bukkit;
 
 public class Main extends JavaPlugin {
-    public static void main(String[] args) {
-        System.out.println("STFUUU");
-        System.out.println("Fehler!");
-    }
+    
 }
